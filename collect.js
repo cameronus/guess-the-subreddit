1bbfeac14c5eceb34fc125410718f1763479c0b3
Cameron Jones <cameroncjones4@gmail.com>
2017-12-19 23:03:33 -0800

    Specific subreddit collection and duplicate prevention

--- collect.js
+++ collect.js
@@ -11,38 +11,38 @@ const approved_ext = ['jpg', 'png', 'gif', 'jpeg', 'JPG', 'PNG']
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/guess-the-subreddit')
 
-collect(30)
+collect(100, 'all')
 
-function collect(pages) {
+function collect(pages, sub) {
   let ids = []
   Post.find({}, (err, posts) => {
     if (err) throw err
     for (const post of posts) {
       ids.push(post.id)
     }
-    collect_posts(pages, '', 0, count => {
+    collect_posts(pages, sub, '', 0, ids, count => {
       console.log(count + ' posts added to the database.')
     })
   })
 }
 
-function collect_posts(pages, after, count, cb) {
-  collect_from_page(after, (a, c) => {
+function collect_posts(pages, sub, after, count, ids, cb) {
+  collect_from_page(sub, after, ids, (a, c) => {
     if (pages == 1) return cb(count + c)
-    collect_posts(pages - 1, a, count + c, cb)
+    collect_posts(pages - 1, sub, a, count + c, ids, cb)
   })
 }
 
-function collect_from_page(after, cb) {
+function collect_from_page(sub, after, ids, cb) {
   if (after != '') after = '?after=' + after
-  request('https://www.reddit.com/.json' + after, (err, response, body) => {
+  request(`https://www.reddit.com/r/${sub}.json${after}`, (err, response, body) => {
     if (err) throw err
     const parsed = JSON.parse(body).data
     const posts = parsed.children
     let count = 0
     for (const raw_post of posts) {
       const post = raw_post.data
-      if (check_post(post)) {
+      if (check_post(post, ids)) {
         count++
         const entry = new Post({
           id: post.id,
@@ -63,6 +63,6 @@ function collect_from_page(after, cb) {
   })
 }
 
-function check_post(post) {
-  return approved_domains.indexOf(post.domain) > -1 && approved_ext.indexOf(post.url.substring(post.url.length - 3)) > -1
+function check_post(post, ids) {
+  return approved_domains.indexOf(post.domain) > -1 && approved_ext.indexOf(post.url.substring(post.url.length - 3)) > -1 && ids.indexOf(post.id) == -1 && !post.over_18
 }
