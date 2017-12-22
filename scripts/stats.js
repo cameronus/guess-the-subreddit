a4217388aaa4ebeb693275cbc2a606e03a51b445
Cameron Jones <cameroncjones4@gmail.com>
2017-12-22 01:22:53 -0800

    Stats script

---
+++ scripts/stats.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+
+const Post = require('../models/Post')
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/guess-the-subreddit')
+
+avg_score()
+
+function avg_score() {
+  let total = 0
+  Post.find({}, (err, posts) => {
+    if (err) throw err
+    for (const post of posts) {
+      total += post.score
+    }
+    console.log('Average Score:', total/posts.length)
+    console.log('Post Count:', posts.length)
+  })
+}
