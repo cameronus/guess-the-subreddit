68ba5f143fbb4034854a913e17258ce7bd4f6e14
Austin Hou <austin@austinhou.com>
2017-12-24 13:31:38 -0800

    Moved requestFullScreen

--- static/js/main.js
+++ static/js/main.js
@@ -9,28 +9,6 @@ $(document).ready(() => {
   img = document.getElementById('img')
 })
 
-function requestFullScreen() {
-
-  var el = document.documentElement;
-
-  // Supports most browsers and their versions.
-  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen
-  || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-  if (requestMethod) {
-    // Native full screen.
-    requestMethod.call(el);
-  }
-  else if (typeof window.ActiveXObject !== "undefined") {
-    // Older IE.
-    var wscript = new ActiveXObject("WScript.Shell");
-
-    if (wscript !== null) {
-      wscript.SendKeys("{F11}");
-    }
-  }
-}
-
 function start(mode) {
   gamemode = mode
   get_challenge(() => {
@@ -158,4 +136,26 @@ function server_error(err) {
   setTimeout(() => {
     window.location.reload(false)
   }, 1500)
+}
+
+function requestFullScreen() {
+
+  var el = document.documentElement;
+
+  // Supports most browsers and their versions.
+  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen
+  || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+  if (requestMethod) {
+    // Native full screen.
+    requestMethod.call(el);
+  }
+  else if (typeof window.ActiveXObject !== "undefined") {
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
+  }
 }
