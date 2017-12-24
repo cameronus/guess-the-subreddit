1b2cb1289c98edeff933f2e336e7192d398c28d7
Austin Hou <austin@austinhou.com>
2017-12-24 00:04:43 -0800

    Goes fullscreen in competetive mode

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,28 @@ $(document).ready(() => {
   img = document.getElementById('img')
 })
 
+function requestFullScreen() {
+
+  var el = document.documentElement;
+
+  // Supports most browsers and their versions.
+  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen
+  || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+  if (requestMethod) {
+    // Native full screen.
+    requestMethod.call(el);
+  }
+  else if (typeof window.ActiveXObject !== "undefined") {
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
+  }
+}
+
 function start(mode) {
   gamemode = mode
   get_challenge(() => {
